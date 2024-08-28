e98fd202fe56dbaa90adc2577d312d6dc7f03c3d
fiqriturhamz <smandakotser@gmail.com>
2024-08-29 03:48:46 +0700

    profile & profile properties file

--- src/test/java/com/programmer/zaman/now/belajar/spring/config/properties/environment/EnvironmentTest.java
+++ src/test/java/com/programmer/zaman/now/belajar/spring/config/properties/environment/EnvironmentTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
 
 @SpringBootTest(classes = EnvironmentTest.TestApplication.class)
 public class EnvironmentTest {
