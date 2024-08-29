6539927bfc7115e7f2124f003b7f9da3caee4bf8
fiqriturhamz <smandakotser@gmail.com>
2024-08-30 00:12:55 +0700

    collection configuration properties

--- src/main/java/com/programmer/zaman/now/belajar/spring/config/properties/properties/ApplicationProperties.java
+++ src/main/java/com/programmer/zaman/now/belajar/spring/config/properties/properties/ApplicationProperties.java
@@ -2,12 +2,18 @@ package com.programmer.zaman.now.belajar.spring.config.properties.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("application")
 public class ApplicationProperties {
     private String name;
     private Integer version;
     private boolean productionMode;
     private DatabaseProperties databaseProperties;
+
+    private List<Role> defaultRoles;
+    private Map<String, Role> roles;
 
     public String getName() {
         return name;
@@ -41,6 +47,21 @@ public class ApplicationProperties {
         this.databaseProperties = databaseProperties;
     }
 
+    public List<Role> getDefaultRoles() {
+        return defaultRoles;
+    }
+
+    public void setDefaultRoles(List<Role> defaultRoles) {
+        this.defaultRoles = defaultRoles;
+    }
+
+    public Map<String, Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Map<String, Role> roles) {
+        this.roles = roles;
+    }
 
     public static class DatabaseProperties {
 
@@ -48,6 +69,26 @@ public class ApplicationProperties {
         private String password;
         private String url;
         private String database;
+
+        private List<String> whiteListTables;
+        private Map<String, Integer> maxTableSize;
+
+
+        public List<String> getWhiteListTables() {
+            return whiteListTables;
+        }
+
+        public void setWhiteListTables(List<String> whiteListTables) {
+            this.whiteListTables = whiteListTables;
+        }
+
+        public Map<String, Integer> getMaxTableSize() {
+            return maxTableSize;
+        }
+
+        public void setMaxTableSize(Map<String, Integer> maxTableSize) {
+            this.maxTableSize = maxTableSize;
+        }
 
         public String getUsername() {
             return username;
@@ -79,6 +120,27 @@ public class ApplicationProperties {
 
         public void setDatabase(String database) {
             this.database = database;
+        }
+    }
+
+    public static class Role {
+        private String id;
+        private String name;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
     }
 }
--- src/test/java/com/programmer/zaman/now/belajar/spring/config/properties/configurationproperties/ConfigurationPropertiesTest.java
+++ src/test/java/com/programmer/zaman/now/belajar/spring/config/properties/configurationproperties/ConfigurationPropertiesTest.java
@@ -9,12 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+
 @SpringBootTest(classes = ConfigurationPropertiesTest.TestApplication.class)
 public class ConfigurationPropertiesTest {
     @Autowired
     private ApplicationProperties applicationProperties;
+
     @Test
-    void testConfigurationProperties(){
+    void testConfigurationProperties() {
         Assertions.assertEquals("Belajar Spring Boot Config Properties", applicationProperties.getName());
         Assertions.assertEquals(1, applicationProperties.getVersion());
         Assertions.assertEquals(false, applicationProperties.isProductionMode());
@@ -22,18 +25,40 @@ public class ConfigurationPropertiesTest {
     }
 
     @Test
-    void testDatabaseProperties(){
+    void testDatabaseProperties() {
         Assertions.assertEquals("fiqri", applicationProperties.getDatabaseProperties().getUsername());
         Assertions.assertEquals("rahasia", applicationProperties.getDatabaseProperties().getPassword());
         Assertions.assertEquals("belajar", applicationProperties.getDatabaseProperties().getDatabase());
         Assertions.assertEquals("jdbc:contoh", applicationProperties.getDatabaseProperties().getUrl());
     }
 
+    @Test
+    void testCollection() {
+        Assertions.assertEquals(Arrays.asList("products", "customers", "categories"), applicationProperties.getDatabaseProperties().getWhiteListTables());
+        Assertions.assertEquals(100, applicationProperties.getDatabaseProperties().getMaxTableSize().get("products"));
+        Assertions.assertEquals(100, applicationProperties.getDatabaseProperties().getMaxTableSize().get("categories"));
+        Assertions.assertEquals(100, applicationProperties.getDatabaseProperties().getMaxTableSize().get("customers"));
+    }
+
+    @Test
+    void testEmbeddedCollection() {
+        Assertions.assertEquals("default", applicationProperties.getDefaultRoles().get(0).getId());
+        Assertions.assertEquals("Default Role", applicationProperties.getDefaultRoles().get(0).getName());
+        Assertions.assertEquals("guest", applicationProperties.getDefaultRoles().get(1).getId());
+        Assertions.assertEquals("Guest Role", applicationProperties.getDefaultRoles().get(1).getName());
+
+        Assertions.assertEquals("admin", applicationProperties.getRoles().get("admin").getId());
+        Assertions.assertEquals("Admin Role", applicationProperties.getRoles().get("admin").getName());
+        Assertions.assertEquals("finance", applicationProperties.getRoles().get("finance").getId());
+        Assertions.assertEquals("Finance Role ", applicationProperties.getRoles().get("finance").getName());
+    }
+
+
     @SpringBootApplication
     @EnableConfigurationProperties({
             ApplicationProperties.class
     })
-    public static class TestApplication{
+    public static class TestApplication {
 
     }
 }
